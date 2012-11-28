029aeeb3b276da9ada0bfa9fc581fa5e689e26ad
Dirk Geurs <dirklectisch@mac.com>
2012-11-28 18:24:09 +0100

    Add monthly subscription

--- src-cljs/p2pfoundation/donate.cljs
+++ src-cljs/p2pfoundation/donate.cljs
@@ -1,17 +1,39 @@
 (ns p2pfoundation.donate
   (:require [domina :as dom])
   (:use [domina.xpath :only [xpath]]
-        [domina.events :only [listen! target]]))
+        [domina.events :only [listen! target]]
+        [domina.css :only [sel]]))
 
 (dom/set-value! (xpath "//input[@name='donationAmount']") "15")
+             
+(defn hide-element [id]
+      (dom/set-styles! (sel (str "#" id))
+                       {:visibility "collapse" :display "none"}))
+                       
+(defn show-element [id]
+     (dom/set-styles! (sel (str "#" id))
+                      {:visibility "visible" :display "block"}))
+             
+(defn handle-frequency-change [evt]
+      (let [freq (dom/value (:target evt))]
+           (if (= freq "oneTime")
+               (do (show-element "oneTimeForm")
+                   (hide-element "monthlyForm"))
+               (do (show-element "monthlyForm")
+                   (hide-element "oneTimeForm")))))
+            
+(listen! (xpath "//input[@name='DonateFrequency']") 
+         :change
+         handle-frequency-change)
 
 (defn set-new-amount [amount]
-      (dom/set-value! (xpath "//input[@name='amount']") amount))
+      (do (dom/set-value! (xpath "//input[@name='amount']") amount)
+          (dom/set-value! (xpath "//input[@name='a3']") amount)
+          (dom/set-value! (xpath "//input[@name='donationAmount']") amount)))
 
 (defn handle-button-click [evt]
       (let [btn (:target evt)]
-      (do (dom/set-value! (xpath "//input[@name='donationAmount']") (dom/text btn))
-          (set-new-amount (dom/text btn)))))
+           (set-new-amount (dom/text btn)) ))
 
 (listen! (xpath "//a[@class='blue_btn']") 
          :click 
@@ -23,6 +45,4 @@
       
 (listen! (xpath "//input[@name='donationAmount']")
          :change
-         handle-amount-change)         
-
-;;(js/alert "Hello from ClojureScript!")
+         handle-amount-change)
